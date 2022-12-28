795e2c90f783914eed4fed77ad37502adaf030a4
shihao888 <sheraton@sina.com>
2022-12-28 16:59:40 +0800

    init

---
+++ aaa.py
@@ -0,0 +1,5 @@
+# Author: 史浩 浙江金融职业学院
+# import sys
+# from PySide6 import QtWidgets, QtCore, QtGui
+# from PySide6.QtWidgets import QWidget, QListWidget, QListWidgetItem, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
+# --------------------------------------------------------
